7b1d840ce11cde2caf251badbb24b563dbeac716
Victoria <dmitrieva.vb.92@gmail.com>
2021-03-12 16:27:46 +0300

    Add files

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('#portfolio').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return '<a class="source_link" href="'+item.el.attr('data-source')+'" target="_blank">'+item.el.attr('data-source')+'</a>';
+			}
+		}
+	});
+});
